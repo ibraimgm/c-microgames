30e37d4239e1a444088c0d388af5e3446304bc5d
Rafael Ibraim <ibraim.gm@gmail.com>
2015-05-08 15:55:37 -0300

    Project skeleton

---
+++ .dirlocals.el
@@ -0,0 +1,12 @@
+((nil
+  ;; Avoid wrong encoding on Windows
+  (buffer-file-coding-system . utf-8-unix)
+  ;; Needed data for project/license snippets
+  (project-name . "C Microgames")
+  (project-release-years . "2015")
+  ;; This is also required, but you might want to
+  ;; set it on yor local emacs configuration, not in the
+  ;; project settings
+  (project-user-name . "Rafael Ibraim")
+  (project-user-mail . "ibraim.gm@gmail.com")
+  ))
---
+++ mg-guess/src/main.c
@@ -0,0 +1,12 @@
+// -*- coding:utf-8-unix; -*-
+
+#include <stdio.h>
+#include "config.h"
+
+int main(int argc, char **argv)
+{
+  printf(APP_HEADER);
+  printf("It worked.\n");
+
+  return 0;
+}
