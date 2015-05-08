a547b254f3381605ed13bdb2419284a082ce9475
Rafael Ibraim <ibraim.gm@gmail.com>
2015-05-08 17:27:07 -0300

    Added license information

---
+++ README.md
@@ -0,0 +1,16 @@
+# C Microgames
+
+A collection of small console games, written in C.
+
+The idea is to implement a series of small games in C, with increasing complexity and review/update my knowledge on the language itself.
+Some of the games are very basic in concept, but I will try to make a little twist in each one of them, to keep things interesting.
+
+So far, there's nothing to show, except the project structure.
+
+## Why?
+
+Because I feel the need to brush of my old C skills. Also, it's a good opportunity to learn [CMake](http://www.cmake.org/).
+The point here is to review (relearn?) C and play some simple games.
+
+## License
+GPLv3. Please, see `LICENSE`.
--- mg-guess/src/main.c
+++ mg-guess/src/main.c
@@ -1,4 +1,23 @@
 // -*- coding:utf-8-unix; -*-
+/*
+ * main.c
+ * Copyright 2015 Rafael Ibraim <ibraim.gm@gmail.com>
+ *
+ * This file is part of C Microgames.
+ *
+ * C Microgames is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * C Microgames is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with C Microgames.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <stdio.h>
 #include "config.h"
